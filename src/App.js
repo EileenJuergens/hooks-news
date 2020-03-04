1e829682bcb17e4810ff6713994bb0fd00013d9f
Eileen Juergens <jurgens@strands.com>
2020-03-04 16:30:57 +0100

    intital set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	// inside is the effect function
+	// reaching out to an external API is a side effect
+	useEffect(() => {
+
+	})
+
+	return (
+		<div>
+			App
+    	</div>
+	);
 }
-
-export default App;
